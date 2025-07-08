98e3b01190b4add2c465e53ebf2b08b412b5db2a
panos <pplakid@csd.auth.gr>
2025-07-08 20:08:53 +0300

    door placement done

--- utilities/World manager/level_manager.gd
+++ utilities/World manager/level_manager.gd
@@ -1,8 +1,16 @@
 class_name LevelManager
 extends Node2D
 
-const gridX = 10
-const gridY = 10
+@export var gridX = 10
+@export var gridY = 10
+@export var roomCount = 10
+
+var directions = [
+		Vector2(0, -1),  # up
+		Vector2(0, 1),   # down
+		Vector2(-1, 0),  # left
+		Vector2(1, 0)    # right
+	]
 
 @onready var rng = RandomNumberGenerator.new()
 var gridSize = Vector2(gridX, gridY)
@@ -21,7 +29,7 @@ func _ready():
 
 func _process(delta: float) -> void:
 	pass
-	
+
 func _on_room_change():
 	print("Leaving...")
 
@@ -44,42 +52,79 @@ func placeRoom(x: int, y: int):
 			newRoom.setPosition(x, y)
 			newRoom.setSpawn(x, y)
 			newRoom.exit.connect(_on_room_change)
-			#newRoom.disableGates()
 			grid[y][x] = newRoom
 	print("Placed room at: (", x, ", ", y, ")")
 
-func choosePosition(x: int, y: int) -> Vector2:
-	var currentRoom := Vector2(x, y)
-	var directions = [
-		Vector2(0, -1),  # up
-		Vector2(0, 1),   # down
-		Vector2(-1, 0),  # left
-		Vector2(1, 0)    # right
-	]
+func generateDoors(currentRoom: Room, queue: Array):
+	
+	var countDoors = 0
+	
+	
+	if queue.is_empty():
+		countDoors = rng.randi_range(1,4)
+	else:
+		countDoors = rng.randi_range(1,3)
 	
 	var count = 0
-	while 1:
-		var dir = directions[rng.randi() % directions.size()]
-		var nextRoom := Vector2(currentRoom.x + dir.x, currentRoom.y + dir.y)
-		if nextRoom.x >= 0 and nextRoom.x < gridX and nextRoom.y >= 0 and nextRoom.y < gridY and grid[nextRoom.y][nextRoom.x] == null:
-			return nextRoom
-		if count == 67+2+420:
-			return Vector2(nextRoom.x-dir.x,nextRoom.y-dir.y)
+	
+	var visited = []
+	
+	while count <= countDoors:
+		var i = rng.randi_range(0,3)
+		var dir = directions[i]
+		var newRoom = Vector2(currentRoom.gridIndex.x + dir.x,currentRoom.gridIndex.y + dir.y)
+		
+		if visited.size() >= 4:
+			break
+		if !dir in visited:
+			visited.push_back(dir)
+		else:
+			continue
+		if newRoom.x < 0 or newRoom.x >= gridX or newRoom.y < 0 or newRoom.y >= gridY:
+			continue
+		if grid[newRoom.y][newRoom.x] != null:
+			continue
+		if currentRoom.doorBitMap[i] == 1:
+			continue
+		if newRoom in queue:
+			continue
+		
+		currentRoom.doorBitMap[i] = 1
+		queue.push_back(newRoom)
 		count += 1
-	return Vector2(-1,-1)
 
 func generateFloor():
-	var roomCount = 9
+	var queue = []
 	var start := Vector2(rng.randi_range(gridX / 3, 2 * gridX / 3), rng.randi_range(gridY / 3, 2 * gridY / 3))
 	placeRoom(start.x, start.y)
+	roomCount -= 1
 	currentRoom = grid[start.y][start.x]
-	var nextRoom :Vector2 = choosePosition(start.x, start.y)
+	generateDoors(currentRoom,queue)
+	var nextRoom : Vector2
 	
-	while (roomCount > 0 && nextRoom.x != -1):
+	while (roomCount > 0 && !queue.is_empty()):
+		nextRoom = queue.pop_front()
 		placeRoom(nextRoom.x, nextRoom.y)
-		nextRoom = choosePosition(nextRoom.x, nextRoom.y)
+		generateDoors(grid[nextRoom.y][nextRoom.x],queue)
 		roomCount -= 1
 	
+	connectDoors()
+
+func connectDoors():
+	for row in grid:
+		for room in row:
+			if !room:
+				continue
+			for i in range(directions.size()):
+				var nextRoom = Vector2(room.gridIndex.x + directions[i].x,room.gridIndex.y + directions[i].y)
+				if nextRoom.x < 0 or nextRoom.x >= gridX or nextRoom.y < 0 or nextRoom.y >= gridY:
+					continue
+				var neigbourRoom = grid[room.gridIndex.y + directions[i].y][room.gridIndex.x + directions[i].x]
+				if neigbourRoom != null:
+					room.doorBitMap[i] = 1
+				else:
+					room.doorBitMap[i] = 0
+
 func renderMap():
 	for child in self.get_children():
 		if child is Room:
@@ -89,16 +134,6 @@ func renderMap():
 			if grid[j][i] == null:
 				continue
 			self.add_child(grid[j][i])
-			#var room = Sprite2D.new()
-			#room.texture = load("res://icon.svg")
-			#room.scale.x *= 0.5
-			#room.scale.y *= 0.5
-			#var width = room.texture.get_size().x * room.scale.x
-			#var height = room.texture.get_size().y * room.scale.y
-			#room.global_position = Vector2(i * width + width / 2,j * height + height / 2)
-			#grid[j][i].add_child(room)
-			
-			
 			var room = grid[j][i]
 			var width = room.width
 			var height = room.height
